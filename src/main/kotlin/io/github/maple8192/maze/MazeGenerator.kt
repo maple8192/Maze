be60fe6c64918ae120d5be90822e4e7740da9918
Maple8192 <57043077+maple8192@users.noreply.github.com>
2021-10-29 00:04:33 +0900

    必要になったときに迷路を生成するように変更

--- src/main/kotlin/io/github/maple8192/maze/MazeGenerator.kt
+++ src/main/kotlin/io/github/maple8192/maze/MazeGenerator.kt
@@ -7,7 +7,17 @@ class MazeGenerator(private val width: Int, private val height: Int) {
     private val path = mutableListOf<Point>()
     private val deadPoint = mutableListOf<Point>()
 
+    private var created = false
+
     var field = Array(width) { Array(height) { 0 } }
+        public get() {
+            if (!created) {
+                makeMaze()
+                created = true
+            }
+
+            return field
+        }
         private set
 
     init {
@@ -21,11 +31,14 @@ class MazeGenerator(private val width: Int, private val height: Int) {
                 }
             }
         }
-
-        makeMaze()
     }
 
     fun run() {
+        if (!created) {
+            makeMaze()
+            created = true
+        }
+
         for (x in 0 until width) {
             for (y in 0 until height) {
                 if (field[x][y] == 1) {
